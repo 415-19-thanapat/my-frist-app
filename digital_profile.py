69ac0d832a49ba172763dd51b2a2a472fa9a671b
415-19-thanapat <54771@yupparaj.ac.th>
2026-06-25 09:04:06 +0700

    digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("My Digital Profile")
+st.header("แนะนำตัว")
+st.write("นายธนพัฒ นามธง")
+st.write("ชื่อเล่น ปอนด์")
+st.write("เลขที่ 19 ม.4/15")
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย : **อยากเรียนด้านการทำอาหารและกาแฟ")
+st.write("อยากเป็นในอนาคต: ขายกาแฟ (Café Owner)")
