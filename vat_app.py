9294cc53603f262f0e085635a5ed9a2dbafa5b2e
415-19-thanapat <54771@yupparaj.ac.th>
2026-08-27 08:59:09 +0700

    vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,8 +1,8 @@
-st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-vat = price * 0.07
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - va
+vat = price * 0.07
+net_price = price - vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.divider()
 st.write("นายธนพัฒ  นามธง เลขที่ 19  ม.4/15")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
